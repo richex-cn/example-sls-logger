31f357d70ccee776d797c50179b63c18c8b32fb9
richex-cn <hzxtiger@vip.qq.com>
2021-12-20 17:58:38 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+import defaultData from './default-data'
+import Logger from 'js-sls-logger'
+import { ref } from 'vue'
+
+const input = ref(JSON.stringify(defaultData, null, 2))
+
+const logger = new Logger({
+  host: 'cn-shenzhen.log.aliyuncs.com',
+  project: 'cent',
+  logstore: 'web',
+  time: 1,
+  count: 10,
+  compress: true
+})
+
+const log = () => {
+  logger.send(input.value)
+}
+
+const bigData = () => {
+  const obj = Object.assign(JSON.parse(JSON.stringify(defaultData)), {
+    data: {
+      test: '0'.repeat(9e5)
+    }
+  })
+  input.value = JSON.stringify(obj, null, 2)
+}
+</script>
+
+<template>
+  <textarea :rows="10" v-model="input" />
+  <button @click="bigData()">Write BigData</button>
+  <button @click="log()">Send</button>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 22px;
+}
+
+textarea {
+  display: block;
+  width: 80vw;
+  height: 50vh;
+}
+</style>
---
+++ src/default-data.js
@@ -0,0 +1,26 @@
+export default {
+  evt: 'navigate',
+  route: {
+    fullPath: '/',
+    hash: '',
+    meta: {
+      title: '页面不存在'
+    },
+    name: 'NotFound',
+    params: {
+      pathMatch: '/'
+    },
+    path: '/',
+    query: {}
+  },
+  data: {
+    hello: 123
+  },
+  a: 'https://aliyun.com/',
+  d: 1639991719458,
+  u: '00000000000000000000000000000000',
+  r: 'jlr',
+  v: '2.3.1',
+  referer: '',
+  local_datetime: '2021-12-20 17:15:19'
+}
